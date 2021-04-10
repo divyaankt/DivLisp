0755e01708a53800f63e2390d527d007e7860a0d
Divyaank Tiwari <divyaankt21@gmail.com>
2021-04-10 20:58:44 +0530

    Initialized parser for DivLisp using mpc

--- parsing.c
+++ parsing.c
@@ -34,6 +34,22 @@ void add_history(char *unused) {}
 int main(int argc, char **argv)
 {
 
+    /* Create Some Parsers */
+    mpc_parser_t *Number = mpc_new("number");
+    mpc_parser_t *Operator = mpc_new("operator");
+    mpc_parser_t *Expr = mpc_new("expr");
+    mpc_parser_t *Lispy = mpc_new("lispy");
+
+    /* Define them with the following Language */
+    mpca_lang(MPCA_LANG_DEFAULT,
+              "                                                     \
+    number   : /-?[0-9]+/ ;                             \
+    operator : '+' | '-' | '*' | '/' ;                  \
+    expr     : <number> | '(' <operator> <expr>+ ')' ;  \
+    lispy    : /^/ <operator> <expr>+ /$/ ;             \
+    ",
+              Number, Operator, Expr, Lispy);
+
     /*Print version and Exit Option*/
     puts("DivLisp version 0.1");
     puts("Press Ctrl+C to Exit\n");
@@ -54,6 +70,8 @@ int main(int argc, char **argv)
         /*Free retrieved input*/
         free(input);
     }
+
+    mpc_cleanup(4, Number, Operator, Expr, Lispy);
 
     return 0;
 }

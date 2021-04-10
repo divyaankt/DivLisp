9f5bf8762967ea69b8f1b72789dc6a67cfc19afe
Divyaank Tiwari <divyaankt21@gmail.com>
2021-04-10 19:53:55 +0530

    Used macros for handling different OSes

--- prompt.c
+++ prompt.c
@@ -1,9 +1,34 @@
 #include <stdio.h>
 #include <stdlib.h>
+
+//If we are compiling on a Windows, include these functions
+#ifdef _WIN32
+#include <string.h>
+
+//Custom implementation of readline()
+char *readline(char *prompt)
+{
+    fputs(prompt, stdout);
+    fgets(buffer, 2048, stdin);
+
+    char *cpy = malloc(strlen(buffer) + 1);
+
+    strcpy(cpy, buffer);
+
+    cpy[strlen(cpy) - 1] = '\0';
+
+    return cpy;
+}
+
+//Custom implementation of add_history()
+void add_history(char *unused) {}
+
+#else
 //readline is used to read input from some prompt, while allowing for editing of that input.
 #include <editline/readline.h>
 //add_history lets us record the history of inputs so that they can be retrieved with the up and down arrows.
 #include <editline/history.h>
+#endif
 
 int main(int argc, char **argv)
 {

8caae595f858004232f0152bb0c6d201e79b533c
Divyaank Tiwari <divyaankt21@gmail.com>
2021-04-12 23:19:56 +0530

    Added methods to construct S-Expressions from input

--- sexpressions.c
+++ sexpressions.c
@@ -64,7 +64,7 @@ typedef struct lval
 } lval;
 
 /* Construct a pointer to a new Number lval */
-lval *lval_num(long x)
+lval *lval_num(double x)
 {
     lval *v = malloc(sizeof(lval));
     v->type = LVAL_NUM;
@@ -134,6 +134,68 @@ void lval_del(lval *v)
 
     /*Free the memory allocated for the "lval" struct itself*/
     free(v);
+}
+
+lval *lval_add(lval *v, lval *x)
+{
+    v->count++;
+    v->cell = realloc(v->cell, sizeof(lval *) * v->count);
+    v->cell[v->count - 1] = x;
+    return v;
+}
+
+lval *lval_read_num(mpc_ast_t *t)
+{
+    /* Check if there is some error in conversion */
+    errno = 0;
+    double x = strtod(t->contents, NULL);
+    return errno != ERANGE ? lval_num(x) : lval_err("Invalid Number!!");
+}
+
+lval *lval_read(mpc_ast_t *t)
+{
+
+    /*If String or Number return conversion to that type*/
+    if (strstr(t->tag, "number"))
+    {
+        return lval_read_num(t);
+    }
+
+    if (strstr(t->tag, "symbol"))
+    {
+        return lval_sym(t->contents);
+    }
+
+    /*If root(>) or sexpr then create empty list*/
+    lval *x = NULL;
+    if (strcmp(t->tag, ">") == 0)
+    {
+        x = lval_sexpr();
+    }
+    if (strstr(t->tag, "sexpr"))
+    {
+        x = lval_sexpr();
+    }
+
+    /* Fill this list with any valid expression contained within */
+    for (int i = 0; i < t->children_num; i++)
+    {
+        if (strcmp(t->children[i]->contents, "(") == 0)
+        {
+            continue;
+        }
+        if (strcmp(t->children[i]->contents, ")") == 0)
+        {
+            continue;
+        }
+        if (strcmp(t->children[i]->tag, "regex") == 0)
+        {
+            continue;
+        }
+        x = lval_add(x, lval_read(t->children[i]));
+    }
+
+    return x;
 }
 
 /* Print an "lval" */
